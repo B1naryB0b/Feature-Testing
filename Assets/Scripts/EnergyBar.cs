581266cf7888f41c2ee1dae2aef2bc232f9b3b1f
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2018-12-14 19:01:32 +0000

    Energy Bar

---
+++ Assets/Scripts/EnergyBar.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public float drainRate = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        slider.value = slider.maxValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value = Time.deltaTime * drainRate;
+    }
+}
