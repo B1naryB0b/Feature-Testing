fc97b095e9a05f968d172e75af652f66be95f9e0
B1naryB0b <35399675+B1naryB0b@users.noreply.github.com>
2018-12-17 10:14:53 +0000

    Negligibility Calculations
    -Found out that parts of the system need to be reworked if they are to be scalable in the long-term e.g. using a game manager for certain game elements

--- Assets/Scripts/GravityBehaviour.cs
+++ Assets/Scripts/GravityBehaviour.cs
@@ -7,7 +7,7 @@ public class GravityBehaviour : MonoBehaviour
     //setting the initial velocity of the gameobject
     public Vector2 initialVel;
     //Gravitational constant is used as one of the multipliers for gravitational field strength
-    public float gravitationalConstant;
+    float gravitationalConstant;
     //All entities that exert a force on the object
     public GameObject[] entities;
 
@@ -28,9 +28,14 @@ public class GravityBehaviour : MonoBehaviour
     //The size of the entities array
     int entityArrayLength;
 
+    MassScalingScript massScaling;
+    GameManagerScript gameManager;
+
     // Start is called before the first frame update
     void Start()
     {
+        gravitationalConstant = gameManager.GetComponent<GameManagerScript>().gravitationalConstant;
+
         //Calls a void function to initialise all of the entities
         InitialisationForEachEntity();
 
@@ -46,6 +51,7 @@ public class GravityBehaviour : MonoBehaviour
 
         //Initialises the Rigidbody2D of the gameobject
         rb = gameObject.GetComponent<Rigidbody2D>();
+
         //sets the initial velicity of the gameobject
         rb.velocity = initialVel;
       
@@ -86,33 +92,43 @@ public class GravityBehaviour : MonoBehaviour
             {
                 //Sets the position and rigidbody component of the entity at the point in the array index i
                 entityTransform[i] = entity.transform.position;
-                if (entity.GetComponent<Rigidbody2D>() != null)
-                    rbEntity[i] = entity.GetComponent<Rigidbody2D>();
 
-                //Calculates the displacement from the entity by finding the magnitude of the difference of 2 vectors:
-                //Its current transform and the transform of the other entity
                 displacement[i] = (entityTransform[i] - gameObject.transform.position).magnitude;
-                //Calculates the direction of the force
-                forceDirection = entityTransform[i] - gameObject.transform.position;
-            
-                //Calculates the magnitude of the force using the equation Gm1m2/d^2
-                //Where: 
-                //m1 is the gameobject mass
-                //m2 is the mass of the other entity
-                //d is displacement
-                //and G is the gravitational constant that can be tweaked to adjust the strength of the gravitational field
-                forceMagnitude = gravitationalConstant * ((rb.mass * rbEntity[i].mass) / Mathf.Pow(displacement[i], 2));
 
-                //This condition is used to prevent NaN errors cuased by dividing by 0
-                //Checks to see if the displacement is not zero
-                //if the displacement is zero then the magnitude of the force is set to 0
-                if (displacement[i] == 0)
-                    forceMagnitude = 0;
+                massScaling = entity.GetComponent<MassScalingScript>();
 
-                //Applies the for on the gameobjects rigidbody as a normalised direction multiplied by the magnitude of the force
-                rb.AddForce(forceDirection.normalized * forceMagnitude);
-                //increments the index
-                 i += 1;
+                if (displacement[i] < massScaling.negligibilityDistance)
+                {
+                    if (entity.GetComponent<Rigidbody2D>() != null)
+                        rbEntity[i] = entity.GetComponent<Rigidbody2D>();
+
+                    //Calculates the displacement from the entity by finding the magnitude of the difference of 2 vectors:
+                    //Its current transform and the transform of the other entity
+                    //Calculates the direction of the force
+                    forceDirection = entityTransform[i] - gameObject.transform.position;
+
+                    //Calculates the magnitude of the force using the equation Gm1m2/d^2
+                    //Where: 
+                    //m1 is the gameobject mass
+                    //m2 is the mass of the other entity
+                    //d is displacement
+                    //and G is the gravitational constant that can be tweaked to adjust the strength of the gravitational field
+                    forceMagnitude = gravitationalConstant * ((rb.mass * rbEntity[i].mass) / Mathf.Pow(displacement[i], 2));
+
+                    //This condition is used to prevent NaN errors cuased by dividing by 0
+                    //Checks to see if the displacement is not zero
+                    //if the displacement is zero then the magnitude of the force is set to 0
+                    if (displacement[i] == 0)
+                        forceMagnitude = 0;
+
+                    //Applies the for on the gameobjects rigidbody as a normalised direction multiplied by the magnitude of the force
+                    rb.AddForce(forceDirection.normalized * forceMagnitude);
+                    Debug.Log("Force Frame: " + Time.frameCount + " Force Acting: " + (forceDirection.normalized * forceMagnitude));
+
+                    //increments the index
+                    i += 1;
+                }
+               
             }
         }
 
